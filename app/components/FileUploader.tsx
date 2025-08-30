78f9b8f33209e28a6468ebbcbcf764b36666f912
Himans <himansbhardwaj1566@gmail.com>
2025-08-30 21:01:27 +0530

    update

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -39,14 +39,17 @@ const FileUploader = ({onFileSelect}: FileUploaderProps) => {
             <p className='text-sm text-gray-500'>
               {formatSize(file.size)}
             </p>
-            </div>
-           
+            </div>      
           </div>
+          <button className='p-2 cursor-pointer' onClick={(e)=>{onFileSelect?. (null) 
+          }}>
+            <img src='/icons/cross.svg' alt='remove' className='w-4 h-4'/>
+          </button>
           </div>
           
         ):(
           <div>
-            <div className='mx-auto w-16 h-16 flex items-center justify-center'>
+            <div className='mx-auto w-16 h-16 flex items-center justify-center mb-2'>
           <img src='/icons/info.svg' alt='upload' className='size-20'/>
         </div>
             <p className='text-lg text-gray-500'>
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,8 +1,10 @@
 import { useState, type FormEvent } from 'react'
 import FileUploader from '~/components/FileUploader'
 import Navbar from '~/components/Navbar'
+import { usePuterStore } from '~/lib/puter'
  
  const upload = () => {
+   const {auth, isLoading, fs, ai, kv} = usePuterStore()
    const [isProcessing, setIsProcessing] = useState(false)
    const [statusText, setStatusText] = useState('')
    const [file, setFile] = useState<File | null>(null)
@@ -11,8 +13,26 @@ import Navbar from '~/components/Navbar'
     setFile(file)
    }
 
-   const handledSubmit =(e : FormEvent<HTMLFormElement>)=>{
+   const handleAnalyze = async ({companyName,jobTitle,jobDescription,file} : {companyName: string, jobTitle : string, jobDescription : string, file : File}) =>{
 
+    setIsProcessing(true);
+    setStatusText("Uploading the file...")
+
+    const uploadedFile = await fs.upload([file]);
+   }
+   const handledSubmit =(e : FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const form = e.currentTarget.closest('form');
+        if(!form) return ;
+        const formData = new FormData(form);
+
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
+
+        if (!file) return;
+        
+        handleAnalyze({companyName, jobTitle, jobDescription, file});
    }
    return (
       <main className="bg-[url('/images/bg-main.svg')] bg-cover">
